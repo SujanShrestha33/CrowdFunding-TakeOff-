9ccc7bfbabd566ac52a77a1e074f2d6f2ef16601
Sujan Shrestha <Sujan.shrestha@agile.com.np>
2024-02-19 16:20:38 +0545

    latest-push-push

--- Client-App/Backend/dbConnection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const URL = "mongodb://localhost:27017/crowdfunding-takeoff";
-
-function connectDb() {
-  return mongoose.connect(URL);
-}
-
-module.exports = connectDb;
