69b74f6b1bbad9bef0d9339a2b4c962ee68d9a4f
PrasannaJung <prasannajung08@gmail.com>
2024-03-04 15:38:11 +0545

    Google auth configuration

--- Backend/index.js
+++ Backend/index.js
@@ -29,34 +29,45 @@ passport.use(
       clientID:
         "136277636263-k8omr4stsgjlhsem6t326d8i70bn8i56.apps.googleusercontent.com",
       clientSecret: "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",
-      callbackURL: "http://localhost:8080/auth/google/callback",
+      callbackURL: "/auth/google/callback",
+      scope: ["profile", "email"],
     },
     async function (accessToken, refreshToken, profile, cb) {
       console.log(profile);
-      const user = await User.create({
-        username: profile.displayName,
-        email: profile.emails[0].value,
-        password: profile.id,
-      });
-      cb(null, user);
+      // try {
+      //   console.log(profile);
+      //   const user = await User.create({
+      //     username: profile.displayName,
+      //     email: profile.emails[0].value,
+      //     password: profile.id,
+      //   });
+      //   return cb(null, user);
+      // } catch (e) {
+      //   return cb(error, null);
+      // }
     },
   ),
 );
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
 app.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["profile", "email"] }),
-  (req, res) => {
-    // You can set the redirect URL dynamically based on your frontend URL
-    const redirectUrl = "http://localhost:4200"; // Update with your actual frontend URL
-    res.redirect(redirectUrl);
-  }
 );
 
 app.get(
   "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/user/login" }),
-
+  passport.authenticate("google", {
+    failureRedirect: "http://localhost:4200/auth/login",
+    successRedirect: "http://localhost:4200/dashboard",
+  }),
 );
 
 const authRoutes = require("./routes/auth");
