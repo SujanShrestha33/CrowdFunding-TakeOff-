49dceb83b8a9953e9deb718889ef8b03aa2535e6
PrasannaJung <prasannajung08@gmail.com>
2024-03-30 11:40:38 +0545

    Made changes in esewa controller

--- Backend/controllers/esewa.js
+++ Backend/controllers/esewa.js
@@ -3,10 +3,11 @@ const Project = require("../models/Project");
 const Reward = require("../models/Reward");
 const Investor = require("../models/Investor");
 const User = require("../models/User");
+const fetch = require("node-fetch");
 
 exports.createInvestmentOrder = async (req, res, next) => {
   const { investmentAmount } = req.body;
-  const projectId = req.params.pid + "a";
+  const projectId = req.params.pid + "abcd";
   console.log(
     "The project id and invest amount  is ",
     projectId,
@@ -28,6 +29,7 @@ exports.createInvestmentOrder = async (req, res, next) => {
     total_amount: investmentAmount,
     transaction_uuid: projectId,
   };
+
   res.json({
     message: "Order Created Sucessfully",
     formData,
@@ -51,11 +53,52 @@ exports.verifyPayment = async (req, res, next) => {
       .map((field) => `${field}=${decodedData[field] || ""}`)
       .join(",");
     console.log(message);
-    const signature = this.createSignature(message);
 
-    if (signature !== decodedData.signature) {
-      res.json({ message: "integrity error" });
+    if (decodedData.signature !== "COMPLETE") {
+      return res.redirect(
+        `http://localhost:4200/project-view/${decodedData.transaction_uuid}/new`,
+      );
     }
+    const projectId = decodedData.transaction_uuid;
+    const project = await Project.findById(decodedData.transaction_uuid);
+    const investedAmount = Number(decodedData.total_amount);
+    console.log("The investment amount is " + investedAmount);
+
+    if (!project) {
+      return res
+        .status(400)
+        .json({ message: "Please provide a valid project id" });
+    }
+
+    if (investedAmount < project.minimumInvestment) {
+      return res.status(400).json({
+        message: `The minimum investment amount should be ${project.minimumInvestment}`,
+      });
+    }
+
+    project.currentAmount = project.currentAmount + investedAmount;
+
+    const investor = await User.findById(req.userId);
+    investor.token = investor.token + 1;
+
+    const rewards = await Reward.find({
+      projectId: projectId,
+      rewardAmount: { $lte: investedAmount },
+    });
+
+    const newInvestor = new Investor({
+      projectId,
+      investorId: req.userId,
+      investedAmount,
+      rewards: rewards,
+    });
+
+    newInvestor.rewards.concat(rewards);
+    console.log(newInvestor);
+
+    await newInvestor.save();
+
+    return res.redirect("http://localhost:4200/main/investment-success");
   } catch (err) {
     console.log(err);
     return res.status(400).json({ error: err?.message || "No Orders found" });
