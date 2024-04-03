48e8176de068fdfccd24a582f87d7459f08d2d0e
PrasannaJung <prasannajung08@gmail.com>
2024-04-03 11:19:04 +0545

    Made minor changes in Project model

--- Backend/models/Project.js
+++ Backend/models/Project.js
@@ -25,7 +25,7 @@ const ProjectSchema = new mongoose.Schema({
   },
   category: {
     type: String,
-    enum: ["Art", "Fashion", "Technology", "Food", "Music", "Games", "Books"],
+    enum: ["Art", "Fashion", "Technology", "Others", "Music", "Games", "Books"],
   },
   startDate: {
     type: Date,
